f38daa2a179c575682cb24db40bde4da173d7123
Artemka091102 <Ponomarev.Artem02@gmail.com>
2018-06-10 14:24:42 +0300

    Test api

--- public/ontour/main.php
+++ public/ontour/main.php
@@ -1,6 +1,5 @@
 <?php
-	if ($_GET["name"] == "Artem")			
-		include "main.html";
-	else
-		include "mainnone.html";
-?>
+if ($_GET["name"] == "Artem")
+    include "main.html";
+else
+    include "mainnone.html";
