6631e962a5ff848c1812f9602bea096f72d9e486
Artemka091102 <Ponomarev.Artem02@gmail.com>
2018-06-11 13:04:23 +0300

    5b1be1452859cbf0bab87e1a Ознакомиться с MapAPI Яндекс расширена карта

--- frontend/index.php
+++ frontend/index.php
@@ -1,7 +1,8 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
     <title>Быстрый старт. Размещение интерактивной карты на странице</title>
-    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <link rel="stylesheet" type="text/css" href="style.css">
     <script src="https://api-maps.yandex.ru/2.1/?lang=ru_RU" type="text/javascript">
     </script>
     <script type="text/javascript">
@@ -9,7 +10,7 @@
         var myMap,
             myPlacemark;
 
-        function init(){
+        function init() {
             myMap = new ymaps.Map("map", {
                 center: [55.76, 37.64],
                 zoom: 7
@@ -21,38 +22,13 @@
             });
 
             myMap.geoObjects.add(myPlacemark);
-            myMap.controls.remove('fullscreenControl');
-            myMap.controls.add('SearchControl', {
-                float: 'left',
-                // «Панель маршрута» будет первой слева.
-                floatIndex: '400'
-            });
-
         }
     </script>
-    <style>
-        body {
-            margin: 0;
-        }
-        #map {
-            width: 100%;
-            height: 100%;
-            position: absolute;
-            z-index: 0;
-        }
-        #header {
-            width: 100%;
-            height: 100px;
-            position: absolute;
-            z-index: 1;
-            opacity: 0.5;
-        }
-    </style>
 </head>
 
 <body>
 <div id="map"></div>
-<div id="header"><h1>fv,jdfnvjkndfjv</h1></div>
+<div id="header"></div>
 </body>
 
-</html>s
+</html>
