4e4ddad982fbfc2e846cc335a01b14110ec58185
Artemka091102 <Ponomarev.Artem02@gmail.com>
2018-06-11 12:22:33 +0300

    5b1be1452859cbf0bab87e1a Ознакомиться с MapAPI Яндекс расширена карта

--- public/ontour/index.php
+++ public/ontour/index.php
@@ -1,88 +1,58 @@
-<!DOCTYPE html>
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
     <title>Быстрый старт. Размещение интерактивной карты на странице</title>
-    <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
     <script src="https://api-maps.yandex.ru/2.1/?lang=ru_RU" type="text/javascript">
     </script>
     <script type="text/javascript">
         ymaps.ready(init);
+        var myMap,
+            myPlacemark;
 
-        function init() {
-            var myPlacemark,
-                myMap = new ymaps.Map('map', {
-                    center: [55.753994, 37.622093],
-                    zoom: 9
-                }, {
-                    searchControlProvider: 'yandex#search'
-                });
-
-            // Слушаем клик на карте.
-            myMap.events.add('click', function (e) {
-                var coords = e.get('coords');
-
-                // Если метка уже создана – просто передвигаем ее.
-                if (myPlacemark) {
-                    myPlacemark.geometry.setCoordinates(coords);
-                }
-                // Если нет – создаем.
-                else {
-                    myPlacemark = createPlacemark(coords);
-                    myMap.geoObjects.add(myPlacemark);
-                    // Слушаем событие окончания перетаскивания на метке.
-                    myPlacemark.events.add('dragend', function () {
-                        getAddress(myPlacemark.geometry.getCoordinates());
-                    });
-                }
-                getAddress(coords);
+        function init(){
+            myMap = new ymaps.Map("map", {
+                center: [55.76, 37.64],
+                zoom: 7
             });
 
-            // Создание метки.
-            function createPlacemark(coords) {
-                return new ymaps.Placemark(coords, {
-                    iconCaption: 'поиск...'
-                }, {
-                    preset: 'islands#violetDotIconWithCaption',
-                    draggable: true
-                });
-            }
+            myPlacemark = new ymaps.Placemark([55.76, 37.64], {
+                hintContent: 'Москва!',
+                balloonContent: 'Столица России'
+            });
 
-            // Определяем адрес по координатам (обратное геокодирование).
-            function getAddress(coords) {
-                myPlacemark.properties.set('iconCaption', 'поиск...');
-                ymaps.geocode(coords).then(function (res) {
-                    var firstGeoObject = res.geoObjects.get(0);
+            myMap.geoObjects.add(myPlacemark);
+            myMap.controls.remove('fullscreenControl');
+            myMap.controls.add('SearchControl', {
+                float: 'left',
+                // «Панель маршрута» будет первой слева.
+                floatIndex: '400'
+            });
 
-                    myPlacemark.properties
-                        .set({
-                            // Формируем строку с данными об объекте.
-                            iconCaption: [
-                                // Название населенного пункта или вышестоящее административно-территориальное образование.
-                                firstGeoObject.getLocalities().length ? firstGeoObject.getLocalities() : firstGeoObject.getAdministrativeAreas(),
-                                // Получаем путь до топонима, если метод вернул null, запрашиваем наименование здания.
-                                firstGeoObject.getThoroughfare() || firstGeoObject.getPremise()
-                            ].filter(Boolean).join(', '),
-                            // В качестве контента балуна задаем строку с адресом объекта.
-                            balloonContent: "" +
-                            "<div align='center'>" +
-                            "   <h3>" + firstGeoObject.getAddressLine() + "</h3>" +
-                            "   <form method='get'>" +
-                            "       Название<br>" +
-                            "       <input type='text' name='name'><br>" +
-                            "       Описание<br>" +
-                            "       <textarea name='dis'></textarea><br>" +
-                            "       <input type='submit'>" +
-                            "   </form>" +
-                            "</div>"
-                        });
-                });
-            }
         }
     </script>
+    <style>
+        body {
+            margin: 0;
+        }
+        #map {
+            width: 100%;
+            height: 100%;
+            position: absolute;
+            z-index: 0;
+        }
+        #header {
+            width: 100%;
+            height: 100px;
+            position: absolute;
+            z-index: 1;
+            opacity: 0.5;
+        }
+    </style>
 </head>
 
 <body>
-<div id="map" style="width: 100%; height: 400px"></div>
+<div id="map"></div>
+<div id="header"><h1>fv,jdfnvjkndfjv</h1></div>
 </body>
 
 </html>
