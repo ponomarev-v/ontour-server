ecfe5a0ccd72fee5f7c7848bfbfaaf13830c7d10
ponomarev-v <ponomarev@kvantorium33.ru>
2018-06-13 12:59:04 +0300

    Проверка отображения ошибок

--- backend/1.php
+++
@@ -1,6 +0,0 @@
-<?php
-print_r($_REQUEST);
-if($_REQUEST['lr'] == 192) {
-    echo "!!!!";
-}
-echo "Привет!";
---
+++ backend/api/vg.php
@@ -0,0 +1,9 @@
+<?php
+namespace API {
+    class VG
+    {
+        public function AA() {
+            return 123;
+        }
+    }
+}
