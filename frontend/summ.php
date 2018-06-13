cdb7be870e37ab2ee6d266ff382401381bfe257e
drongood <ggs33@yandex.ru>
2018-06-13 14:42:12 +0300

    c

--- frontend/summ.php
+++ frontend/summ.php
@@ -24,7 +24,7 @@
             {
                     data = eval("(" + data + ")");
                     if(data.result == "success") {
-                        alert("Hi");
+                        alert(data);
                 }
             }
         });
