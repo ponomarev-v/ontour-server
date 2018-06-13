5e35ed69e661280341a3a6460ab32b784a07ef56
artem-grigoriev <googl.dr.02.12345@gmail.com>
2018-06-13 13:36:20 +0300

    class

---
+++ backend/api/ArtemClass.php
@@ -0,0 +1,12 @@
+<?php
+    namespase API{
+        class ART{
+            public function __construct()
+            {
+                $b = \Utils::Request('b');
+                $a = \Utils::Request('a');
+                return ($a + $b);
+            }
+        }
+};
+?>
