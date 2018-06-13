ef52eb04424ecec56f5893d7d31cc0c3549438e9
DaryaBoonkina <35730548+DaryaBoonkina@users.noreply.github.com>
2018-06-13 14:01:14 +0300

    commit

--- backend/api/emil.php
+++ backend/api/emil.php
@@ -3,9 +3,8 @@ namespace API {
     class bg
     {
         public function bg() {
-            $a = \Utils::Request('a');
-            $b = \Utils::Request('b');
-            return $a+$b;
+           
+            return 123;
         }
     }
 }
