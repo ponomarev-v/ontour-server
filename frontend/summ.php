d8eb749d2e55af8fd4500c331872d32a7fa4e886
drongood <ggs33@yandex.ru>
2018-06-13 14:33:15 +0300

    commit

--- frontend/summ.php
+++ frontend/summ.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <script src="/js/jquery-3.3.1.js" type="text/javascript"></script>
     <title>Document</title>
 </head>
 <body>
