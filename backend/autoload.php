a2dcfcf74a1cea4e63138bc3eb0a99aad9a2b606
ponomarev-v <ponomarev@kvantorium33.ru>
2018-06-13 18:15:52 +0300

    Проверка отображения ошибок

--- backend/autoload.php
+++ backend/autoload.php
@@ -4,7 +4,6 @@ function load_class($class_name)
     $items = explode('\\', strtolower($class_name));
     foreach(array('lib', 'modules') as $item) {
         $path = implode(DIRECTORY_SEPARATOR, array_merge(array($item), $items)) . '.php';
-        echo $path."\n";
         if(file_exists($path)) {
             require_once $path;
             return true;
