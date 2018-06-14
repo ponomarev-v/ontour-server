4bc5eb9a4ca7cc08ccf3d13d1895d4771a03cc98
ponomarev-v <ponomarev@kvantorium33.ru>
2018-06-14 17:32:17 +0300

    https://trello.com/c/7yBlpJuG

--- backend/autoload.php
+++ backend/autoload.php
@@ -1,12 +1,13 @@
 <?php
 define("ROOT_PATH", __DIR__);
+
 require_once(ROOT_PATH . '/config.php');
 
 function load_class($class_name)
 {
     $items = explode('\\', strtolower($class_name));
     foreach(array('lib', 'modules') as $item) {
-        $path = implode(DIRECTORY_SEPARATOR, array_merge(array($item), $items)) . '.php';
+        $path = implode(DIRECTORY_SEPARATOR, array_merge(array(ROOT_PATH, $item), $items)) . '.php';
         if(file_exists($path)) {
             require_once $path;
             return true;
@@ -16,7 +17,7 @@ function load_class($class_name)
 
 function load_api_class($class_name)
 {
-    $path = implode(DIRECTORY_SEPARATOR, array('api', $class_name)) . '.php';
+    $path = implode(DIRECTORY_SEPARATOR, array(ROOT_PATH, 'api', $class_name)) . '.php';
     if(file_exists($path)) {
         require_once $path;
         return true;
