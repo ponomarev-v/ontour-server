1abbec8fd1eaa2802c3c7f54bf3cb79f89637126
Artemka091102 <Ponomarev.Artem02@gmail.com>
2018-06-14 18:39:58 +0300

    скрипты теперь в php

--- frontend/login.php
+++ frontend/login.php
@@ -25,3 +25,93 @@
         </div>
     </div>
 </div>
+<script>
+    function createProfile(user_info) {
+        $("#menu_login").hide();
+        $("#menu_register").hide();
+        $("#menu_logout").show();
+        $("#menu_profile").show();
+        $("#menu_main").show();
+    }
+
+    $(document).ready(function() {
+        $('#phone').mask('8(000)000-00-00');
+        $.ajax({
+            type: "POST",
+            url: "http://ontourapi.kvantorium33.ru/?method=user.info",
+            xhrFields: {withCredentials: true},
+
+            success: function(data)
+            {
+                data = eval("(" + data + ")");
+                if(data.result == "success") {
+                    createProfile(data);
+                }
+            }
+        });
+
+        $("#btn_logout").click(function () {
+            $.ajax({
+                type: "POST",
+                url: "http://ontourapi.kvantorium33.ru/?method=user.logout",
+                xhrFields: {withCredentials: true},
+                success: function(data)
+                {
+                    data = eval("(" + data + ")");
+                    if(data.result == "success") {
+                        $("#menu_logout").hide();
+                        $("#menu_profile").hide();
+                        $("#menu_register").show();
+                        $("#menu_login").show();
+                        $("#menu_main").hide();
+                    }
+                }
+            });
+        });
+
+        $("#btn_login").click(function () {
+            $("#login_window").show();
+        });
+
+        $("#login_window .close").click(function () {
+            $("#login_window").hide();
+        });
+
+        $("#login_window").click(function (e) {
+            if(e.target == this)
+                $("#login_window").hide();
+        });
+
+        $("#login_form").submit(function(e) {
+            $.ajax({
+                type: "POST",
+                url: "http://ontourapi.kvantorium33.ru/?method=user.login",
+                data: $("#login_form").serialize(),
+                xhrFields: {withCredentials: true},
+                success: function(data)
+                {
+                    data = eval("(" + data + ")");
+                    if(data.result == "success") {
+                        $("#login_error").html("");
+                        $("#login_window").hide();
+                        $("#menu_main").show();
+                        createProfile(data);
+                    } else {
+                        $("#login_error").html("Неправильный логин или пароль");
+                    }
+                }
+            });
+            e.preventDefault();
+        });
+
+        $("#btn_profile").click(function () {
+            $("#profile_window").show();
+        });
+
+        $("#btn_register").click(function () {
+            $("#login_window").hide();
+            $("#register_window").show();
+
+        });
+    });
+</script>
--- frontend/profile.php
+++ frontend/profile.php
@@ -22,3 +22,66 @@
         </div>
     </div>
 </div>
+<script>
+    $(document).ready(function() {
+        $('#phone').mask('8(000)000-00-00');
+        $.ajax({
+            type: "POST",
+            url: "http://ontourapi.kvantorium33.ru/?method=user.info",
+            xhrFields: {withCredentials: true},
+            success: function (data) {
+                data = eval("(" + data + ")");
+                if (data.result == "success") {
+                    createProfile(data);
+                }
+            }
+        });
+        $("#profile_window .close").click(function () {
+            $("#profile_window").hide();
+        });
+        $("#profile_window").click(function (e) {
+            if (e.target == this)
+                $("#profile_window").hide();
+        });
+        $("#profile_form").submit(function (e) {
+            $.ajax({
+                type: "POST",
+                url: "http://ontourapi.kvantorium33.ru/?method=user.profile",
+                data: $("#profile_form").serialize(),
+                xhrFields: {withCredentials: true},
+                success: function (data) {
+                    data = eval("(" + data + ")");
+                    if (data.result == "success") {
+                        $("#profile_error").html("");
+                        $("#profile_window").hide();
+                        $("#menu_main").show();
+                        createProfile(data);
+                    } else {
+                        $("#profile_error").html("Неправильный логин или пароль");
+                    }
+                }
+            });
+            e.preventDefault();
+        });
+
+        $("#profile_window").click(function (e) {
+            if (e.target == this)
+                $("#profile_window").hide();
+        });
+
+        $("#btn_change_email").click(function () {
+            $("#btn_change_email").show();
+            $("#profile_window").hide();
+        });
+
+        $("#btn_change_password").click(function () {
+            $("#btn_change_password").show();
+            $("#profile_window").hide();
+        });
+
+        $("#btn_change_phone").click(function () {
+            $("#btn_change_phone").show();
+            $("#profile_window").hide();
+        });
+    });
+</script>
