b3d68d831faba54897348466a14b1c8e7cc9c11d
ponomarev-v <ponomarev@kvantorium33.ru>
2018-06-14 17:40:09 +0300

    https://trello.com/c/7yBlpJuG

--- backend/services/smssender.php
+++ backend/services/smssender.php
@@ -4,7 +4,8 @@ $db = \Core::DB();
 while(true) {
     $res = $db->where('date_send', 0)->get('sms');
     foreach($res as $sms) {
-        exec('gammu --sendsms text +7' . $sms['phone'] . ' -text "' . $sms['message'] . '" -unicode');
+        $command = 'gammu --sendsms text +7' . $sms['phone'] . ' -text "' . $sms['message'] . '" -unicode';
+        exec($command);
         $db->where('id', $sms['id'])->update('sms', array('date_send' => time()));
     }
     sleep(10);
