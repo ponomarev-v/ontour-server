732bfbf8aef12cd61e2670ef9ea1869bc99b3e22
VladMalygin <35717191+VladMalygin@users.noreply.github.com>
2018-06-14 11:00:03 +0300

    c

--- frontend/test.php
+++ frontend/test.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <script src="js/jquery-3.3.1.js"></script>
     <title>Document</title>
 </head>
 <body>
@@ -16,6 +17,6 @@
 </script>
 <p>Конец</p>
 </form>
-<script src="js/jquery-3.3.1.js"></script>
+
 </body>
 </html>
