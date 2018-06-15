cdff22d5f3c2fd5ac07dc91cfbc366072bf0198a
artem-grigoriev <googl.dr.02.12345@gmail.com>
2018-06-15 18:42:56 +0300

    обнова

--- backend/api/map.php
+++ backend/api/map.php
@@ -3,8 +3,13 @@
         class Map{
             public static function add(){
                 $data = array(
-                  ''
+                    'cx' => \Utils::Request('cx'),
+                    'cy' => \Utils::Request('cy'),
+                    'kind' => \Utils::Request('kind'),
+                    'name' => \Utils::Request('kind'),
+                    'description' => \Utils::Request('description')
                 );
+
             }
         }
     }
---
+++ backend/modules/objects.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: kvant
+ * Date: 6/15/18
+ * Time: 6:32 PM
+ */
