f91217563156ddc7d13115e380f4fd85cebe4002
artem-grigoriev <googl.dr.02.12345@gmail.com>
2018-06-15 17:40:16 +0300

    111

--- backend/api/user.php
+++ backend/api/user.php
@@ -66,9 +66,6 @@ namespace API {
             $_SESSION['userid'] = $id;
             return $this->Info();
         }
-
-        public function Myfunc(){
-            return 0;
-        }
+        
     }
 }
