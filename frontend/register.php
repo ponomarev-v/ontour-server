4e2e0dac9c68d5942635cfbb83c5aa16a0ae6d5e
ponomarev-v <ponomarev@kvantorium33.ru>
2018-06-15 15:16:15 +0300

    Разработка метода для получения информации об активном пользователе

--- frontend/register.php
+++ frontend/register.php
@@ -3,13 +3,13 @@
         <span class="close">&times;</span>
         <div id="form" align="center">
             <h1>
-                 Регистрация ON TOUR
+                Регистрация ON TOUR
             </h1>
             <form id="register_form">
-                <input type="text"     name="name"     placeholder="Имя"            class="form" required><br>
-                <input type="text"     name="surname"  placeholder="Фамилия"        class="form" required><br>
-                <input type="password" name="password" placeholder="Пароль"         class="form" required><br>
-                <input type="text"     name="phone"    placeholder="Номер телефона" class="form" required id="phone_register" >
+                <input type="text" name="name" placeholder="Имя" class="form" required><br>
+                <input type="text" name="email" placeholder="Электронная почта" class="form" required><br>
+                <input type="text" name="phone" placeholder="Номер телефона" class="form" required id="phone_register">
+                <input type="password" name="password" placeholder="Пароль" class="form" required><br>
                 <p>
                     <input type="submit" value="Зарегистрироваться">
                 </p>
@@ -18,7 +18,7 @@
     </div>
 </div>
 <script>
-    $(document).ready(function() {
+    $(document).ready(function () {
         $('#phone_register').mask('8(000)000-00-00');
         $(".btn_register").click(function () {
             $("#login_window").hide();
@@ -30,21 +30,20 @@
         });
 
         $("#register_window").click(function (e) {
-            if(e.target == this)
+            if (e.target == this)
                 $("#register_window").hide();
         });
 
-        $("#register_form").submit(function(e) {
+        $("#register_form").submit(function (e) {
 
             $.ajax({
                 type: "POST",
                 url: "http://ontourapi.kvantorium33.ru/?method=user.register",
                 data: $("#register_form").serialize(),
                 xhrFields: {withCredentials: true},
-                success: function(data)
-                {
+                success: function (data) {
                     data = eval("(" + data + ")");
-                    if(data.result == "success") {
+                    if (data.result == "success") {
                         $("#register_error").html("");
                         $("#register_window").hide();
                         $("#menu_main").show();
