3fa6a24c03a1fb955078a37f3ff062ad29c85bfd
Artemka091102 <Ponomarev.Artem02@gmail.com>
2018-06-16 16:29:50 +0300

    переименовал yandex_map ы district_map

--- frontend/index.php
+++ frontend/index.php
@@ -1,4 +1,4 @@
 <?php
 include "header.php";/*подключение головы сайта*/
-include "yandex_map.php";/*подключение тела сайта*/
+include "district_map.php";/*подключение тела сайта*/
 include "footer.php";/*подключение ног сайта*/
