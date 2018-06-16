a88908595d51fa7b2dc2d82bbc23925d2aad13bb
drongood12 <ggs33@yandex.ru>
2018-06-16 17:16:31 +0300

    fix colors map

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -43,11 +43,19 @@ $(window).on("load",function(){
 
     });
     $("#Far_Eastern",svgDoc).css("fill","#f1ce82");
-	 $("#Siberia",svgDoc).css("fill","#c2cdd3");
-	 $("#Urals",svgDoc).css("fill","#8ecdef");
-	 $("#Northwestern",svgDoc).css("fill","#9eb8f9");
-	 $("#Volga",svgDoc).css("fill","#fbe230");
-	 $("#Central",svgDoc).css("fill","#a1e736");
-	 $("#KL",svgDoc).css("fill","#eea268");
-	 $("#KC",svgDoc).css("fill","#d27387");
+    $("#Far_Eastern",svgDoc).css("stroke","#f1ce82");
+   $("#Siberia",svgDoc).css("fill","#c2cdd3");
+   $("#Siberia",svgDoc).css("stroke","#c2cdd3");
+   $("#Urals",svgDoc).css("fill","#8ecdef");
+    $("#Urals",svgDoc).css("stroke","#8ecdef");
+   $("#Northwestern",svgDoc).css("fill","#9eb8f9");
+    $("#Northwestern",svgDoc).css("stroke","#9eb8f9");
+   $("#Volga",svgDoc).css("fill","#fbe230");
+   $("#Volga",svgDoc).css("stroke","#fbe230");
+   $("#Central",svgDoc).css("fill","#a1e736");
+   $("#Central",svgDoc).css("stroke","#a1e736");
+   $("#KL",svgDoc).css("fill","#eea268");
+   $("#KL",svgDoc).css("stroke","#eea268");
+   $("#KC",svgDoc).css("fill","#d27387");
+   $("#KC",svgDoc).css("stroke","#d27387");
 });
