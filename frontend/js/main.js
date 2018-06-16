3f51b52400ed738a716516234a23b30fa4c13665
Artemka091102 <Ponomarev.Artem02@gmail.com>
2018-06-16 17:24:33 +0300

    небольшие исправления синтаксиса в main.js

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -1,61 +1,46 @@
 $(window).on("load",function(){
-    
     var a = $("#map")[0];
     var svgDoc = a.contentDocument;
-    var obj = ["#Central","#Volga","#Urals","#Siberia","#Far_Eastern","#Northwestern","#KC","#KL","#path4765"]
-    $("#Far_Eastern",svgDoc).css("fill","#1076C8");
-    
+    var obj = ["#Central","#Volga","#Urals","#Siberia","#Far_Eastern","#Northwestern","#KC","#KL","#path4765"];
+    var $data_tooltip;
     obj.forEach(element => {
-        var regionColor = $(element,svgDoc).css("fill","#f1ce82");
-        var borderColor = $(element,svgDoc).css("stroke","#fff");
-        var $data_tooltip;
-
-
-
         $(element,svgDoc).mousemove(function(){
-            var regionColor = $(element,svgDoc).css("opacity","0.7");     
-             
+            var regionColor = $(element,svgDoc).css("opacity","0.7");
         });
         $(element,svgDoc).mouseout(function(){
             var regionColor = $(element,svgDoc).css("opacity","1");
-            
         });
     });
     $("[data-tooltip]",svgDoc).mousemove(function (eventObject) {
-
         $data_tooltip = $(this).attr("data-tooltip");
-        
         $("#tooltip").text($data_tooltip)
                      .css({ 
                          "top" : eventObject.pageY,
                          "left" : eventObject.pageX
                      })
                      .show();
-
     }).mouseout(function () {
-
         $("#tooltip").hide()
                      .text("")
                      .css({
                          "top" : 0,
                         "left" : 0
                      });
-
     });
     $("#Far_Eastern",svgDoc).css("fill","#f1ce82");
     $("#Far_Eastern",svgDoc).css("stroke","#f1ce82");
-   $("#Siberia",svgDoc).css("fill","#c2cdd3");
-   $("#Siberia",svgDoc).css("stroke","#c2cdd3");
-   $("#Urals",svgDoc).css("fill","#8ecdef");
+    $("#Siberia",svgDoc).css("fill","#c2cdd3");
+    $("#Siberia",svgDoc).css("stroke","#c2cdd3");
+    $("#Urals",svgDoc).css("fill","#8ecdef");
     $("#Urals",svgDoc).css("stroke","#8ecdef");
-   $("#Northwestern",svgDoc).css("fill","#9eb8f9");
+    $("#Northwestern",svgDoc).css("fill","#9eb8f9");
     $("#Northwestern",svgDoc).css("stroke","#9eb8f9");
-   $("#Volga",svgDoc).css("fill","#fbe230");
-   $("#Volga",svgDoc).css("stroke","#fbe230");
-   $("#Central",svgDoc).css("fill","#a1e736");
-   $("#Central",svgDoc).css("stroke","#a1e736");
-   $("#KL",svgDoc).css("fill","#eea268");
-   $("#KL",svgDoc).css("stroke","#eea268");
-   $("#KC",svgDoc).css("fill","#d27387");
-   $("#KC",svgDoc).css("stroke","#d27387");
+    $("#Volga",svgDoc).css("fill","#fbe230");
+    $("#Volga",svgDoc).css("stroke","#fbe230");
+    $("#Central",svgDoc).css("fill","#a1e736");
+    $("#Central",svgDoc).css("stroke","#a1e736");
+    $("#KL",svgDoc).css("fill","#eea268");
+    $("#KL",svgDoc).css("stroke","#eea268");
+    $("#KC",svgDoc).css("fill","#d27387");
+    $("#KC",svgDoc).css("stroke","#d27387");
 });
