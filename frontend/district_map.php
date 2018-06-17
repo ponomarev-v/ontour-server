0169219d5dc06aff6f7222157638695214962ab5
Artemka091102 <Ponomarev.Artem02@gmail.com>
2018-06-17 09:11:40 +0300

    в district_map выравнял карту по середине и сделал помельче

--- frontend/district_map.php
+++ frontend/district_map.php
@@ -1,5 +1,7 @@
 <div id="tooltip"></div>
-<object type="image/svg+xml" data="images/Federal_Map.svg" id="federal_map">
-    <p>Ваш браузер не поддерживает svg</p>
-</object>
+<div align="center">
+    <object type="image/svg+xml" data="images/Federal_Map.svg" id="federal_map" width="70%">
+        <p>Ваш браузер не поддерживает svg</p>
+    </object>
+</div>
 <script src="js/federal_map.js"></script>
