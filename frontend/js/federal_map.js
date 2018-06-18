f681708ca8b1d2117dfd08d42e71c0a47a496969
drongood <ggs33@yandex.ru>
2018-06-18 11:38:33 +0300

    commit

--- frontend/js/federal_map.js
+++ frontend/js/federal_map.js
@@ -25,7 +25,7 @@ $(window).on("load",function(){
         
         $("#tooltip").text($data_tooltip)
                      .css({ 
-                         "top" : eventObject.pageY + 85,
+                         "top" : eventObject.pageY + 285,
                         "left" : eventObject.pageX,
                         
                      })
