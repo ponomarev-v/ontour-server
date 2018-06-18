03caea4718f81356e57d0469292f715c43b58bfd
drongood <ggs33@yandex.ru>
2018-06-18 10:53:39 +0300

    fix central map

--- frontend/js/central_map.js
+++ frontend/js/central_map.js
@@ -2,6 +2,18 @@ $(window).on("load",function(){
     var centralMap = $("#central_map")[0];
     var svgCentral = centralMap.contentDocument;
     var objCentral = ["#path96","#path94","#path92","#path46","#path50","#path60","#path68","#path54","#path62","#path4926","#path44","#path78","#path66","#path98","#path74","#path64","#path82","#path42"]
+    var objVolga = ["#path52","#path84","#path88","#path106"]
+    objVolga.forEach(element => {
+        $(element,svgCentral).css("fill","#fbe230");
+        $(element,svgCentral).css("stroke","#fff");
+        $(element,svgCentral).hover(function(){
+            var regionColor = $(element,svgCentral).css("opacity","0.7");     
+        });
+        $(element,svgCentral).mouseout(function(){
+           $(element,svgCentral).css("opacity","1");   
+        });
+    });
+
     objCentral.forEach(element => {
         var regionColor = $(element,svgCentral).css("fill","#a1e736");
         var borderColor = $(element,svgCentral).css("stroke","#71a12a");
