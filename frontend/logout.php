e1c9ca7ea8bfecc43174a162f27a10183bb7b9ca
Artemka091102 <Ponomarev.Artem02@gmail.com>
2018-06-18 09:13:22 +0300

    Выход теперь адекватно работает. Сайт Работает

--- frontend/logout.php
+++ frontend/logout.php
@@ -67,23 +67,5 @@
 
         });
 
-        $("#btn_logout").click(function () {
-            $.ajax({
-                type: "POST",
-                url: "http://ontourapi.kvantorium33.ru/?method=user.logout",
-                xhrFields: {withCredentials: true},
-                success: function(data)
-                {
-                    data = eval("(" + data + ")");
-                    if(data.result == "success") {
-                        $("#menu_logout").hide();
-                        $("#menu_profile").hide();
-                        $("#menu_main").hide();
-                        $("#menu_register").show();
-                        $("#menu_login").show();
-                    }
-                }
-            });
-        });
     });
 </script>
