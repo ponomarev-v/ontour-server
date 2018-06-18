980f96603441b790ad56bf9e79febfb3c4dc6051
drongood <ggs33@yandex.ru>
2018-06-18 11:30:33 +0300

    fix map

--- frontend/js/vladimir_map.js
+++ frontend/js/vladimir_map.js
@@ -19,8 +19,8 @@ $(window).on("load",function(){
         
         $("#tooltip").text($data_tooltip_central)
                      .css({ 
-                         "top" : eventObject.pageY + 45,
-                        "left" : eventObject.pageX + 45,
+                         "top" : eventObject.pageY + 245,
+                        "left" : eventObject.pageX + 245,
                         
                      })
                      .show();
