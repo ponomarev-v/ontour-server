5895360aa1ce89cddaec2edcd1b72474e650ef11
drongood <ggs33@yandex.ru>
2018-06-18 12:08:23 +0300

    ca

--- frontend/js/federal_map.js
+++ frontend/js/federal_map.js
@@ -25,7 +25,7 @@ $(window).on("load",function(){
         
         $("#tooltip").text($data_tooltip)
                      .css({ 
-                         "top" : eventObject.pageY - 135,
+                         "top" : eventObject.pageY - 35,
                         "left" : eventObject.pageX,
                         
                      })
