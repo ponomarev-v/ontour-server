50f9f7ffe04d1596862c482e70bb78c937956c99
drongood <ggs33@yandex.ru>
2018-06-19 11:50:55 +0300

    nothing

--- frontend/js/federal_map.js
+++ frontend/js/federal_map.js
@@ -1,13 +1,9 @@
 $(window).on("load",function(){
     
     var a = $("#federal_map")[0];
-    var allFederals = $("#g4548",a);
-    allFederals = $("#Federals",allFederals);
-    var svgDoc = allFederals.contentDocument;
-    var list = svgDoc.childNodes;
-    for (var i = 0;i < list.length;i++){
-        alert (list[i]);
-    }
+    
+    var svgDoc = a.contentDocument;
+    
     	
     
     var obj = ["#Central","#Volga","#Urals","#Siberia","#Far_Eastern","#Northwestern","#KC","#KL","#path4765"]
