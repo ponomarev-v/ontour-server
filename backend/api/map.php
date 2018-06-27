4625758c712b9a831eaea954ac8aa8297d0248ae
drongood <ggs33@yandex.ru>
2018-06-27 13:30:12 +0300

    fix errors

--- backend/api/map.php
+++ backend/api/map.php
@@ -19,8 +19,8 @@
                 $name = \Utils::Request('name');
                 return \Objects::Get_obj($cx, $cy, $name );
             }
-            public static function GetObjs(){
-                return \Objects::GetAllObj();
-            }
+            // public static function GetObjs(){
+            //     return \Objects::GetAllObj();
+            // }
         }
     }
