2b9fa8d404200124296723146dc1a4c6e3957dc5
Artemka091102 <Ponomarev.Artem02@gmail.com>
2018-06-28 16:34:29 +0300

    вернул кнопки

--- frontend/menu.php
+++ frontend/menu.php
@@ -2,10 +2,10 @@
     <ul class="menu">
         <li id="menu_register"><a href=# class="btn_register">Регистрация</a></li>
         <li id="menu_login"><a href=# id="btn_login">Вход</a></li>
-        <!--<li id = "selet_region"><a href="/district_map.php">Выбрать федеральный округ</a></li>-->
-        <!--<li id="show_map"><a href="/yandex_map.php">Просмотр карты</a></li>-->
         <li id="menu_profile" style="display: none"><a href=# id="btn_profile">Личный кабинет</a></li>
         <li id="menu_logout" style="display: none"><a href=# id="btn_logout">Выход</a></li><br>
+        <li id = "selet_region"><a href="/district_map.php">Выбрать федеральный округ</a></li>
+        <li id="show_map"><a href="/yandex_map.php">Просмотр карты</a></li>
         <li id="menu_main" style="display: none"><a href=#>Меню после входа</a></li>
     </ul>
 </div>
