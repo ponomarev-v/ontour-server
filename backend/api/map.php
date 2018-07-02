eed696f907e85d305b9c83ad3387d18426c01a1e
artem-grigoriev <googl.dr.02.12345@gmail.com>
2018-07-02 15:02:22 +0300

    added find

--- backend/api/map.php
+++ backend/api/map.php
@@ -14,21 +14,29 @@
                     return true;
                 }
             }
+
             public static function Details(){
                 $cx   = \Utils::Request('cx');
                 $cy   = \Utils::Request('cy');
                 $name = \Utils::Request('name');
                 return \Objects::Get_obj($cx, $cy, $name );
             }
+
             public static function GetObjs(){
                 return \Objects::GetAllObj();
             }
+
             public static function Delete(){
                 $cx   = \Utils::Request('cx');
                 $cy   = \Utils::Request('cy');
                 $del = \Utils::Request('delete');
                 if ($del == 1)
                     return \Objects::Delete_obj($cx, $cy);
+            }
+
+            public static function Find_Target(){
+                $str = \Utils::Request('find');
+                return \Objects::Find_obj($str);
             }
         }
     }
