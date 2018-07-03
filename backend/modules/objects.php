daa4fb56a548c57aa0ecc986937b887a388da165
artem-grigoriev <googl.dr.02.12345@gmail.com>
2018-07-03 12:27:36 +0300

    debugging

--- backend/modules/objects.php
+++ backend/modules/objects.php
@@ -70,7 +70,7 @@ class Objects
     public static function Find_obj($str)
     {
         $db = Core::DB();
-        $res = $db->where('name', $str)->get('object');
+        $res = $db->query($str);
         return $res;
     }
 }
