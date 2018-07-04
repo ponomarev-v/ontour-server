abecdebc94877cfa8091e7631c43ca7f8e3f888c
Artemka091102 <Ponomarev.Artem02@gmail.com>
2018-07-04 22:42:06 +0300

    изменил менюшки

--- frontend/menu.php
+++ frontend/menu.php
@@ -3,8 +3,6 @@
         <li id = "selet_region"><a href="/district_map.php">Выбрать федеральный округ</a></li>
         <li id="show_map"><a href="/yandex_map.php">Просмотр карты</a></li>
         <li id="menu_profile" style="display: none"><a href=# id="btn_profile">Личный кабинет</a></li>
-        <li id="menu_logout" style="display: none"><a href=# id="btn_logout">Выход</a></li><br>
-        <li id="menu_main" style="display: none"><a href=#>Меню после входа</a></li>
     </ul>
 </div>
 <?php
