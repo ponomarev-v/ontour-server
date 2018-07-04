f48b95fabd4fd28d915bceab7d0356bda154e123
Artemka091102 <Ponomarev.Artem02@gmail.com>
2018-07-04 22:46:56 +0300

    айдишники...

--- frontend/menu_down.php
+++ frontend/menu_down.php
@@ -1,6 +1,6 @@
-<div id="menu"><!--меню сверху страницы-->
-    <ul class="menu">
-        <li id = "selet_region"><a href="/district_map.php">Выбрать федеральный округ</a></li>
+<div id="menu_down"><!--меню сверху страницы-->
+    <ul class="menu_down">
+        <li id="select_region"><a href="/district_map.php">Выбрать федеральный округ</a></li>
         <li id="show_map"><a href="/yandex_map.php">Просмотр карты</a></li>
         <li id="menu_profile" style="display: none"><a href=# id="btn_profile">Личный кабинет</a></li>
     </ul>
--- frontend/menu_up.php
+++ frontend/menu_up.php
@@ -1,5 +1,5 @@
-<ul class="top_menu">
-    <li><a href="#" class="btn_register">Регистрация</a></li>
-    <li><a href="#" id="btn_login">Вход</a></li>
+<ul class="menu_up">
+    <li id="menu_register"><a href="#" class="btn_register">Регистрация</a></li>
+    <li id="menu_login"><a href="#" id="btn_login">Вход</a></li>
     <li id="menu_logout" style="display: none"><a href=# id="btn_logout">Выход</a></li>
 </ul>
