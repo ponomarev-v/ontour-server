9e76a9e4975d200bfd6c05253b14cc04b9cce80e
Artemka091102 <Ponomarev.Artem02@gmail.com>
2018-07-05 08:34:30 +0300

    чиню

--- frontend/header.php
+++ frontend/header.php
@@ -1,27 +1,25 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
-<head>
-    <title>ON TOUR</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="/css/style.css"><!--общие стили-->
-    <link rel="stylesheet" href="/css/menu.css"><!--стили меню-->
-    <link rel="stylesheet" href="/css/window.css"><!--стили всплывающих окон-->
-    <link rel="stylesheet" href="/css/bootstrap-grid.min.css">
-    <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
-    
-    <script src="/js/jquery-3.3.1.js"></script><!--скрипт jquery-->
-    <script src="/js/jquery.mask.js"></script><!--скрипт jquery.mask-->
-    
+<head>s
+    <title>turneon</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"><!--хрен знает что-->
+    <link rel="stylesheet" href="css/style.css"><!--общие стили-->
+    <link rel="stylesheet" href="css/menu.css"><!--стили меню-->
+    <link rel="stylesheet" href="css/window.css"><!--стили всплывающих окон-->
+    <link rel="stylesheet" href="css/bootstrap-grid.min.css"><!--сетка адаптивной верстки-->
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/><!--кодировка-->
+    <script src="js/jquery-3.3.1.js"></script><!--скрипт jquery-->
+    <script src="js/jquery.mask.js"></script><!--скрипт jquery.mask-->
     <script src="https://api-maps.yandex.ru/2.1/?lang=ru_RU"></script><!--скрипт яндекс.карт-->
 </head>
 <body>
-<div>
-    <a href="/district_map.php" id="logo">
-        <img src="/images/free_horizontal_on_white_by_logaster (3).png" height=120px><!--логотип-->
+<div class="header">
+    <a href="district_map.php" class="logo">
+        <img src="images/free_horizontal_on_white_by_logaster (3).png" height=100%><!--логотип-->
     </a>
-    <div id="header_up">
+    <div class="header_up">
         <?php include "menu_up.php"; ?><!--подключение меню-->
     </div>
-    <div id="header_down">
+    <div class="header_down">
         <?php include "menu_down.php"; ?><!--подключение меню-->
     </div>
 </div>
