03a79c60c38c4a06cdda05ad329d200ff76d1201
Artemka091102 <Ponomarev.Artem02@gmail.com>
2018-07-05 08:45:32 +0300

    id -> class

--- frontend/menu_up.php
+++ frontend/menu_up.php
@@ -1,5 +1,5 @@
-<div id="menu"><!--меню сверху страницы-->
-    <ul class="menu">
+<div class="menu"><!--меню сверху страницы-->
+    <ul class="menu_up">
         <li id="menu_register"><a href=# class="btn_register">Регистрация</a></li>
         <li id="menu_login"><a href=# id="btn_login">Вход</a></li>
         <li id="menu_logout" style="display: none"><a href=# id="btn_logout">Выход</a></li>
