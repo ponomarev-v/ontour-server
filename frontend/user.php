5d1f27e11ba472baba5cb92f14e68305d7c4b159
Artemka091102 <Ponomarev.Artem02@gmail.com>
2018-07-06 15:08:01 +0300

    дописывание

--- frontend/user.php
+++ frontend/user.php
@@ -116,6 +116,7 @@
             $("#menu_register").addClass("hidden");
             $("#menu_login").addClass("hidden");
             $("#menu_logout").removeClass("hidden");
+            $("#menu_profile").removeClass("hidden");
             $("#window_profile form [name='name']").val(user_info["name"]);
             $("#window_profile form [name='age']").val(user_info["age"]);
             $("#window_profile form [name='school']").val(user_info["school"]);
@@ -125,7 +126,6 @@
                 user_info["phone"][3] + user_info["phone"][4] + user_info["phone"][5] + "-" + user_info["phone"][6] +
                 user_info["phone"][7] + "-" + user_info["phone"][8] + user_info["phone"][9];
             $("#window_profile form [name='phone']").val(ph);
-
         } else {
             $("#menu_register").removeClass("hidden");
             $("#menu_login").removeClass("hidden");
@@ -133,22 +133,34 @@
         }
     }
     
-    $(document).ready(function() {
+    function update_user_info() {
         exec_ajax_request({method: "user.info"}, function(data)
         {
             data = eval("(" + data + ")");
             if(data.result == "success") {
                 load_user_info(data);
+            } else {
+                load_user_info(null);
             }
         }, null);
+    }
+    
+    $(document).ready(function() {
+        update_user_info();
 
         register_ajax_form("#window_login form", function (data) {
             data = eval("(" + data + ")");
             if (data.result == "success") {
+                close_active_window();
                 load_user_info(data);
             } else {
                 $("#window_login .error").html(data["message"]);
             }
+        }, null);
+
+        register_ajax_form("#window_logout form", function (data) {
+            close_active_window();
+            update_user_info();
         }, null);
 
         register_ajax_form("#window_register form", function (data) {
