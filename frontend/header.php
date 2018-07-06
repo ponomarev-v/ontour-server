460245345ae53bc62f05543ec8a198cb19d1b1db
Artemka091102 <Ponomarev.Artem02@gmail.com>
2018-07-06 10:57:12 +0300

    откат до реформы

--- frontend/header.php
+++ frontend/header.php
@@ -2,14 +2,13 @@
 <head>
     <title>turneon</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0"><!--хрен знает что-->
-    <link rel="stylesheet" href="/css/style.css"><!--общие стили-->
-    <link rel="stylesheet" href="/css/menu.css"><!--стили меню-->
-    <link rel="stylesheet" href="/css/window.css"><!--стили всплывающих окон-->
-    <link rel="stylesheet" href="/css/bootstrap-grid.min.css"><!--сетка адаптивной верстки-->
+    <link rel="stylesheet" href="css/style.css"><!--общие стили-->
+    <link rel="stylesheet" href="css/menu.css"><!--стили меню-->
+    <link rel="stylesheet" href="css/window.css"><!--стили всплывающих окон-->
+    <link rel="stylesheet" href="css/bootstrap-grid.min.css"><!--сетка адаптивной верстки-->
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/><!--кодировка-->
-    <script src="/js/jquery-3.3.1.js"></script><!--скрипт jquery-->
-    <script src="/js/jquery.mask.js"></script><!--скрипт jquery.mask-->
-    <script src="/js/app.js"></script><!--скрипт app-->
+    <script src="js/jquery-3.3.1.js"></script><!--скрипт jquery-->
+    <script src="js/jquery.mask.js"></script><!--скрипт jquery.mask-->
     <script src="https://api-maps.yandex.ru/2.1/?lang=ru_RU"></script><!--скрипт яндекс.карт-->
 </head>
 <body>
