45f220b75c37afcf762ccc9f8a8507f408dc61c3
artem-grigoriev <googl.dr.02.12345@gmail.com>
2018-07-12 21:07:55 +0300

    debagging change password

--- backend/modules/users.php
+++ backend/modules/users.php
@@ -109,7 +109,7 @@ class Users
                 throw new \Exception("Неправильный старый пароль");
         }*/
         $arr = array('old' => $pass_old, 'new' => $res['password']);
-        return $arr;
+        return $res;
     }
 //проверка данных на обновление userdata
     public static function ChangeUserProfile($id, $data)
