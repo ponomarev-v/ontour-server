9ff6acf4e42a30398f12d61d5543d573e2728dc8
Igor <markov2002@bk.ru>
2018-07-12 15:45:02 +0300

    debugging

--- backend/modules/users.php
+++ backend/modules/users.php
@@ -80,7 +80,7 @@ class Users
     }
 //смена пароля
 //TODO починить
-    public static function ChangePass($id, pass_old, $pass_new)
+    public static function ChangePass($id, $pass_old, $pass_new)
     {
         $db = Core::DB();
         if (!isset($pass_old) || strlen($pass_old) < 8 || strlen($pass_old) > 255) {
