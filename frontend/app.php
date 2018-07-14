649855a9f18ae7f17f99c8fd7c2845367e2436ac
Artemka091102 <Ponomarev.Artem02@gmail.com>
2018-07-15 00:22:12 +0300

    добавлено 2 странички "Главная" и "Приложение"

---
+++ frontend/app.php
@@ -0,0 +1,21 @@
+<?php
+include "header.php";/*подключение головы сайта*/
+?>
+<div class="center">
+    <h1>
+        Приложение
+    </h1>
+    <h3>
+        Бла - Бла про приложение
+    </h3>
+    <p>
+        еще немного Бла - Бла и ссылки на скачивание
+    </p>
+</div>
+<?php
+include "footer.php";/*подключение ног сайта*/
+?>
+
+<script>
+    //$("#window_find_target").hide();
+</script>
---
+++ frontend/main.php
@@ -0,0 +1,21 @@
+<?php
+include "header.php";/*подключение головы сайта*/
+?>
+<div class="center">
+    <h1>
+        Главная
+    </h1>
+    <h3>
+        Немного о проекте
+    </h3>
+    <p>
+        Странно, но тут должен быть текст о нашем проекте
+    </p>
+</div>
+<?php
+include "footer.php";/*подключение ног сайта*/
+?>
+
+<script>
+    //$("#window_find_target").hide();
+</script>
