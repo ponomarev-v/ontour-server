3610edd903875498c98fa47633dd683a7c0b8b10
Igor <markov2002@bk.ru>
2018-07-17 16:30:49 +0300

    тыкаю Location

--- backend/api/user.php
+++ backend/api/user.php
@@ -5,7 +5,7 @@ namespace API {
     {
         public function Location(){
             //return $_SERVER['GEOIP_REGION_NAME'];
-            return $_SERVER['GEOIP_AREA_CODE'];
+            return $_SERVER['REMOTE_ADDR'];
         }
 
         public function Info()
