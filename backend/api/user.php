8e35db2d86b8102ed27730476a8c93e571e2217b
Igor <markov2002@bk.ru>
2018-07-17 16:33:37 +0300

    тыкаю Location

--- backend/api/user.php
+++ backend/api/user.php
@@ -5,7 +5,15 @@ namespace API {
     {
         public function Location(){
             //return $_SERVER['GEOIP_REGION_NAME'];
-            return $_SERVER['REMOTE_ADDR'];
+            $client  = @$_SERVER['HTTP_CLIENT_IP'];
+            $forward = @$_SERVER['HTTP_X_FORWARDED_FOR'];
+            $remote  = @$_SERVER['REMOTE_ADDR'];
+
+            if(filter_var($client, FILTER_VALIDATE_IP)) $ip = $client;
+            elseif(filter_var($forward, FILTER_VALIDATE_IP)) $ip = $forward;
+            else $ip = $remote;
+
+            echo $ip;
         }
 
         public function Info()
