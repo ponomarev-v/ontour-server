44445c433200ae643c6731b461ad44f9921c03b4
Igor <markov2002@bk.ru>
2018-07-19 21:57:44 +0300

    таки подругому будет

--- backend/modules/users.php
+++ backend/modules/users.php
@@ -100,7 +100,7 @@ class Users
         $bd = $res[0];
         $keyNoE['password'] = $bd['password'];
         $keyNoE['key'] = $bd['activate_code'];
-        $key = "md5($keyNoE)";
+        $key = md5("$keyNoE");
         $link = "http://api.turneon.ru/?method=user.EmailVerification&id=" . $user . "&key=" . $key;
 
         $email = $res[0]['email'];
