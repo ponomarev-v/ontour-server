4c0909f828536bada6359ed91404ca335603d9b2
Taruu <markov2002@bk.ru>
2018-07-31 08:38:28 +0300

    тест загрузки файлов

--- backend/test.php
+++ backend/test.php
@@ -4,34 +4,10 @@
 </head>
 <body>
 
-<?php
-if ( ! $_FILES )
-{
-    echo '
-		  <h2>Форма для загрузки файлов</h2>
-		  <form action="" method="post" enctype="multipart/form-data">
-		  <input type="file" name="filename"><br>
-		  <input type="submit" value="Загрузить"><br>
-		  </form>
-	';
-}
-else
-{
-    // Проверяем загружен ли файл
-    if(  is_uploaded_file($_FILES["filename"]["tmp_name"])  )
-    {
-        // Если файл загружен успешно, перемещаем его
-        // из временной директории в конечную
-        move_uploaded_file
-        (
-            $_FILES["filename"]["tmp_name"],
-            __DIR__  .  '/www/'  .  $_FILES["filename"]["name"]
-        );
-    } else {
-        echo("Ошибка загрузки файла");
-    }
-}
-?>
+<form action="/upload.php" method="post" enctype="multipart/form-data">
+    <input type="file" name="file">
+    <input type="submit" value="Отправить">
+</form>
 
 </body>
 </html>
