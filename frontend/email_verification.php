f40c16cbb016e3a951abf73f4f391cd4355d96d8
Artemka091102 <Ponomarev.Artem02@gmail.com>
2018-07-31 15:40:15 +0300

    добавил страничку подтверждения почты

---
+++ frontend/email_verification.php
@@ -0,0 +1,12 @@
+<html>
+<head>
+</head>
+<body>
+Ваша Почта подтверждена
+<a href="index.php">
+    <!--<form>-->
+        <input type="submit" value="Назад" class="button">
+    <!--</form>-->
+</a>
+</body>
+</html>
