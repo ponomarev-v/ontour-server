dfac4265c3422424bf95441df92cbc9b724caf80
artem-grigoriev <googl.dr.02.12345@gmail.com>
2018-08-03 10:13:13 +0300

    sdfsdf

--- frontend/upload.php
+++ frontend/upload.php
@@ -1,11 +1,12 @@
 <?php
-//$uploaddir = 'images/';
+$uploaddir = 'images/';
 // это папка, в которую будет загружаться картинка
-//$apend = date('YmdHis').rand(100,1000).'.jpg';
+$apend=date('YmdHis').rand(100,1000).'.jpg'; 
 // это имя, которое будет присвоенно изображению 
-//$filetoserver = "$uploaddir$apend";
+$uploadfile = "$uploaddir$apend"; 
 //в переменную $uploadfile будет входить папка и имя изображения
-if (move_uploaded_file($_FILES['userfile']['tmp_name'], $filetoserver))
+if (isset($_FILES['userfile']))
     throw new exeption('it work');
 else 
-throw new exeption("it isn't work");
+    throw new exeption("it isn't work");
+?>
