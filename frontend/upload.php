1358264cad02c26c5e0554a645f12c3d5326efb7
Taruu <markov2002@bk.ru>
2018-08-03 10:42:30 +0300

    тест загрузки файлов

--- frontend/upload.php
+++ frontend/upload.php
@@ -10,7 +10,7 @@
 print_r($_FILES);
 
 if(isset($_POST['upload'])){
-    $folder = '/www/turneon-server/upload';
+    $folder = '/www/turneon-server/upload/';
     $uploadedFile = $folder.basename($_FILES['uploadFile']['name']);
     if(is_uploaded_file($_FILES['uploadFile']['tmp_name'])){
         if(move_uploaded_file($_FILES['uploadFile']['tmp_name'],
