aa9f0645e243e7ac42b5fc400136d6b6655ee47c
drongood12 <ggs33@yandex.ru>
2018-08-03 19:48:39 +0300

    debbuging

--- frontend/upload.php
+++ frontend/upload.php
@@ -1,33 +1,27 @@
 <html>
 <head>Загрузка  файлов на сервер</head>
 <body>
-<form  enctype="multipart/form-data" action=""  method="post">
-    <input  type="hidden" name="MAX_FILE_SIZE" value="300000"  />
-    <input  type="file" name="uploadFile"/>
-    <input  type="submit" name="upload" value="Загрузить"/>
-</form>
-<?php
-print_r($_FILES);
+<input type="hidden" name="MAX_FILE_SIZE" value="30000">
+    <form action="upload.php" method="post" enctype="multipart/form-data">
+        <input type="file" name="filename"><br>
+        <input type="submit" value="Загрузить"><br>
+    </form>
+<script>
+//print_r($_FILES);
+$.ajax({
+    type: "POST",
+    url: "http://api.turneon.ru/?method=user.TestFunc",
+    xhrFields: {withCredentials: true},
+    success: function (data) {
+        data = eval("(" + data + ")");
+        if (data.result == "success") {
+           alert("success load file");
+        } else {
+            alert("error load file");
+        }
+    }
+});
 
-if(isset($_POST['upload'])){
-    $folder = '/www/turneon-server/upload/';
-    $uploadedFile = $folder.basename($_FILES['uploadFile']['name']);
-    if(is_uploaded_file($_FILES['uploadFile']['tmp_name'])){
-        if(move_uploaded_file($_FILES['uploadFile']['tmp_name'],
-            $uploadedFile))
-        {
-            echo "Файл загружен";
-  }
-        else
-        {
-            echo "Во  время загрузки файла произошла ошибка";
-  }
-    }
-    else
-    {
-        echo 'Файл не  загружен';
-    }
-}
-?>
+</script>
 </body>
 </html>
