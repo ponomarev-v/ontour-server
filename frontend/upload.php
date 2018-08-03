16d25f999e9716abce6d0cbd38773a6617a402f2
Igor <markov2002@bk.ru>
2018-08-03 10:05:08 +0300

    загрузку делаем

--- frontend/upload.php
+++ frontend/upload.php
@@ -1,13 +1,11 @@
 <?php
-$uploaddir = 'images/';
+//$uploaddir = 'images/';
 // это папка, в которую будет загружаться картинка
-$apend=date('YmdHis').rand(100,1000).'.jpg'; 
+//$apend = date('YmdHis').rand(100,1000).'.jpg';
 // это имя, которое будет присвоенно изображению 
-$uploadfile = "$uploaddir$apend"; 
+//$filetoserver = "$uploaddir$apend";
 //в переменную $uploadfile будет входить папка и имя изображения
-if (move_uploaded_file($_FILES['userfile']['tmp_name'], $uploadfile))
+if (move_uploaded_file($_FILES['userfile']['tmp_name'], $filetoserver))
     throw new exeption('it work');
 else 
 throw new exeption("it isn't work");
- 
-?>
