cb205805d1eb87969e39c1b4ab678c6620dd0fdc
Taruu <markov2002@bk.ru>
2018-08-04 11:16:28 +0300

    тест загрузки файлов

--- frontend/testup.php
+++ frontend/testup.php
@@ -1,7 +1,7 @@
 <!-- Тип кодирования данных, enctype, ДОЛЖЕН БЫТЬ указан ИМЕННО так -->
 <form enctype="multipart/form-data" action="http://api.turneon.ru/?method=user.TestFunc" method="POST">
     <!-- Поле MAX_FILE_SIZE должно быть указано до поля загрузки файла -->
-    <input type="hidden" name="MAX_FILE_SIZE" value="30000" />
+    <input type="hidden" name="MAX_FILE_SIZE" value="3000000000" />
     <!-- Название элемента input определяет имя в массиве $_FILES -->
     Отправить этот файл: <input name="userfile" type="file" />
     <input type="submit" value="Отправить файл" />
