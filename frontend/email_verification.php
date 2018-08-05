5f678aa14c3e692d25868d8143c253fd1c382e3e
Taruu <markov2002@bk.ru>
2018-08-05 18:10:51 +0300

    пишем норм потверждение почты

--- frontend/email_verification.php
+++ frontend/email_verification.php
@@ -4,6 +4,19 @@
 <body>
 Ваша Почта подтверждена
 <a href="index.php">
+    <?php
+    // Создаем поток
+    $opts = array(
+        'http'=>array(
+            'method'=>"GET",
+            'header'=>"Accept-language: en\r\n" .
+                "Cookie: foo=bar\r\n"
+        )
+    );
+
+    $context = stream_context_create($opts);
+    echo $context;
+    ?>
     <!--<form>-->
         <input type="submit" value="Назад" class="button">
     <!--</form>-->
