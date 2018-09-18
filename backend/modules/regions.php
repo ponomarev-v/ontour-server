a71d286a10837c93042ac8cb917fa1c99048cb9b
Taruu <markov2002@bk.ru>
2018-09-18 22:10:43 +0300

    repair

--- backend/modules/regions.php
+++ backend/modules/regions.php
@@ -25,7 +25,7 @@ class Regions{
         echo $idobj;
         $db  = Core::DB();
         $obj = $db->get('object');
-        return $obj-> where($idobj);
+        return $obj-> where('reg',$idobj);
     }
 
 }
