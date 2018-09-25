d8a35ba450515ec8136c80926f23dce5c295cc3c
ponomarev-v <ponomarev@kvantorium33.ru>
2018-09-25 18:00:41 +0300

    Работа с регионами

--- frontend/region.php
+++ frontend/region.php
@@ -6,7 +6,7 @@ include "header.php";
     <h3 class="map_header">
         Россия
     </h3>
-    <object type="image/svg+xml" data="images/district_map.svg" id="federal_map" width="70%">
+    <object type="image/svg+xml" data="images/district_map2.svg" id="federal_map" width="70%">
         <p>Ваш браузер не поддерживает svg</p>
     </object>
 </div>
