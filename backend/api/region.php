29d74c1a40b4c5d5913221b596a8398642d26e72
ponomarev-v <ponomarev@kvantorium33.ru>
2018-10-13 22:01:28 +0300

    Работа с регионами

--- backend/api/region.php
+++ backend/api/region.php
@@ -1,20 +1,26 @@
 <?php
-    namespace API{
-        class Region{
-            
-            public static function Get(){
-                $parent = \Utils::Request('parent');
-                return \Regions::GetReg($parent);
-            }
 
-            //обновляем кол-во объектов 
-            public static function UpdateObjVal(){
-                $reg = \Utils::Request('reg');
-                if(!isset($reg) || empty($reg)){
-                    return \Regions::Update_Obj_Val();
-                } else {
-                    return \Regions::Update_Obj_Val($reg);
-                }
+namespace API {
+    class Region
+    {
+        public static function Get()
+        {
+            $parent = \Utils::Request('parent');
+            if($parent == "auto") {
+                $reg = "";
+            }
+            return \Regions::GetReg($parent);
+        }
+
+        //обновляем кол-во объектов
+        public static function UpdateObjVal()
+        {
+            $reg = \Utils::Request('reg');
+            if(!isset($reg) || empty($reg)) {
+                return \Regions::Update_Obj_Val();
+            } else {
+                return \Regions::Update_Obj_Val($reg);
             }
         }
     }
+}
--- frontend/region.php
+++ frontend/region.php
@@ -1,8 +1,26 @@
 <?php
 include "header.php";
 ?>
-<div>
-
-</div>
+<!-- <?php print_r($_SERVER); ?> -->
+    <div class="center">
+        <h1>Приложение</h1>
+    </div>
+    <script>
+        $.ajax({
+            type: "POST",
+            url: "http://api.turneon.ru/?method=user.GeoIpLocation",
+            xhrFields: {withCredentials: true},
+            success: function (data) {
+                data = eval("(" + data + ")");
+                if (data.result == "success") {
+                    if (data.data == "Vladimir") {
+                        window.location = "/Vladimir_map.php"
+                    } else {
+                        window.location = "/district_map.php"
+                    }
+                }
+            }
+        });
+    </script>
 <?php
 include "footer.php";
