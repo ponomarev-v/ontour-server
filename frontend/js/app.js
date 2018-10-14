0a72083a371de49f0b69e56b86263b106b9d33d2
ponomarev-v <ponomarev@kvantorium33.ru>
2018-10-14 21:29:00 +0300

    Работа с регионами

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -45,6 +45,21 @@ function exec_ajax_request(data, handler, error) {
     });
 }
 
+function getUrlParameter(sParam) {
+    var sPageURL = decodeURIComponent(window.location.search.substring(1)),
+        sURLVariables = sPageURL.split('&'),
+        sParameterName,
+        i;
+
+    for (i = 0; i < sURLVariables.length; i++) {
+        sParameterName = sURLVariables[i].split('=');
+
+        if (sParameterName[0] === sParam) {
+            return sParameterName[1] === undefined ? true : sParameterName[1];
+        }
+    }
+}
+
 $(document).ready(function() {
     $(".close").click(close_active_window);
     $('.phone').mask('8(000)000-00-00');
--- frontend/js/map.js
+++ frontend/js/map.js
@@ -1,121 +1,17 @@
 ymaps.ready(init);
-var myMap,  myPlacemark;
-var get = location.search;
-var x,y;
-var tmp = new Array();
-var tmp2 = new Array();
-var param = new Array();
-if(get != ''){
-    tmp = get.substr(1).split('&');
-    for(var i = 0;i < tmp.length;i++){
-        tmp2 = tmp[i].split('=');
-        param[tmp2[0]] = tmp2[1];
-    }
-}
-
-var town = param['data'];
-
-switch(town){
-    case 'Vladimir':
-    x = 56.129042;
-    y = 40.407030;
-    break;
-
-    case 'Sydogda':
-    x = 55.949879;
-    y = 40.856295;
-    break;
-
-    case 'Kameskovo':
-    x = 56.348916;
-    y = 40.995588;
-    break;
-
-    case 'Syzdal':
-    x = 56.419836;
-    y = 40.449457;
-    break;
-
-    case 'Kovrov':
-    x = 56.363628;
-    y = 41.311220;
-    break;
-
-    case 'Radyznyi':
-    x = 55.996052;
-    y = 40.332281;
-    break;
-    case 'Sobinka':
-    x = 55.993837;
-    y = 40.017943;
-    break;
-
-    case 'Selevanovo':
-    x = 55.870031;
-    y = 41.772074;
-    break;
-
-    case 'Myrom':
-    x = 55.579169;
-    y = 42.052411;
-    break;
-
-    case 'Gorohovec':
-    x = 56.201695;
-    y = 42.691194;
-    break;
-
-    case 'Vazniki':
-    x = 56.247021;
-    y = 42.158862;
-    break;
-
-    case 'Gys-Xrystalnyi':
-    x = 55.619818;
-    y = 40.657902;
-    break;
-
-    case 'Melenki':
-    x = 55.338715;
-    y = 41.634030;
-    break;
-
-    case 'Yuriev-Polskiy':
-    x = 56.499250;
-    y = 39.680814;
-    break;
-
-    case 'Kolcygino':
-    x = 56.293901;
-    y = 39.376042;
-    break;
-
-    case 'Petyshki':
-    x = 55.930967;
-    y = 39.459900;
-    break;
-
-    case 'Kirzach':
-    x = 56.161694;
-    y = 38.872025;
-    break;
-
-    case 'Aleksandrov':
-    x = 56.397774;
-    y = 38.727621;
-    break;
-
-    default:
-    x = 55.755814;
-    y = 37.617635;
-    break;
-}
-
-
-
-var myPlacemark,objectManager;
+var myMap,  myPlacemark, objectManager;
 
 function init () {
+    pt = getUrlParameter('pt');
+    x = 55.753215;
+    y = 37.622504;
+    if(pt) {
+        pt = pt.split(',');
+        if(pt.length == 2) {
+            x = pt[0];
+            y = pt[1];
+        }
+    }
     var myMap = new ymaps.Map('map', {
             center: [x, y],
             zoom: 10
