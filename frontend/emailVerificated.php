aaf33c1847c0c7ca02ac22182caa325c12b45ce0
Artemka091102 <Ponomarev.Artem02@gmail.com>
2018-10-17 23:05:25 +0300

    подтверждение почты

---
+++ frontend/emailVerificated.php
@@ -0,0 +1,12 @@
+<?php
+include "header.php";
+?>
+    <div style="height: 100%">
+        <h1 align="center" style="margin-top: 150px">ВАША ПОЧТА ПОДТВЕРЖДЕНА</h1>
+        <p align="center">
+            Для продолжения работы на сервисе перейдите по
+            <a href="http://turneon.ru">ссылке</a>
+        </p>
+    </div>
+<?php
+include "footer.php";
