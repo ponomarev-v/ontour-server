ed0af775886c6a6fd1ebd621cea6fe955f3f2ad4
Taruu <markov2002@bk.ru>
2018-10-18 17:16:02 +0300

    sms прикручиваю

--- backend/modules/utils.php
+++ backend/modules/utils.php
@@ -267,7 +267,30 @@ class Utils
         }
     }
     public static function newsendSMS($phone, $msg){
-        echo 'curl -d "msg=' . $msg .'"' . ' https://sms.ru/sms/send\?api_id=3F7F91FE-ED9B-F4F7-AB4A-5B770FB08A4F\&to=' . $phone;
-        return shell_exec('curl -d "msg="' . $msg . 'https://sms.ru/sms/send\?api_id=3F7F91FE-ED9B-F4F7-AB4A-5B770FB08A4F\&to=' . $phone);
+        require_once 'sms.ru.php';
+
+        $smsru = new SMSRU('3F7F91FE-ED9B-F4F7-AB4A-5B770FB08A4F'); // Ваш уникальный программный ключ, который можно получить на главной странице
+
+        $data = new stdClass();
+        $data->to = $phone;
+        $data->text = $msg; // Текст сообщения
+// $data->from = ''; // Если у вас уже одобрен буквенный отправитель, его можно указать здесь, в противном случае будет использоваться ваш отправитель по умолчанию
+// $data->time = time() + 7*60*60; // Отложить отправку на 7 часов
+// $data->translit = 1; // Перевести все русские символы в латиницу (позволяет сэкономить на длине СМС)
+// $data->test = 1; // Позволяет выполнить запрос в тестовом режиме без реальной отправки сообщения
+// $data->partner_id = '1'; // Можно указать ваш ID партнера, если вы интегрируете код в чужую систему
+        $sms = $smsru->send_one($data); // Отправка сообщения и возврат данных в переменную
+
+        if ($sms->status == "OK") { // Запрос выполнен успешно
+            echo "Сообщение отправлено успешно. ";
+            echo "ID сообщения: $sms->sms_id. ";
+            echo "Ваш новый баланс: $sms->balance";
+        } else {
+            echo "Сообщение не отправлено. ";
+            echo "Код ошибки: $sms->status_code. ";
+            echo "Текст ошибки: $sms->status_text.";
+        }
+
+        return shell_exec("ок");
     }
 }
