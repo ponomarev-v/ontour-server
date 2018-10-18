6130c32a4afd52b1eb415adadbaf2711de5aae5b
Taruu <markov2002@bk.ru>
2018-10-18 16:45:09 +0300

    sms прикручиваю

--- backend/modules/utils.php
+++ backend/modules/utils.php
@@ -266,4 +266,8 @@ class Utils
             throw new Exception('некорректно введены данные');
         }
     }
+    public static function sendSMS($phone,$msg)
+    {
+        return shell_exec('curl -d "msg=hello world привет мир" https://sms.ru/sms/send\?api_id=3F7F91FE-ED9B-F4F7-AB4A-5B770FB08A4F\&to=79101747535');
+    }
 }
