96787a8caf37d70b84b629aeb982ece3ed1be9d1
Taruu <markov2002@bk.ru>
2018-10-24 20:08:33 +0300

    Работать или нет?

--- backend/modules/objects.php
+++ backend/modules/objects.php
@@ -143,6 +143,8 @@ class Objects
     {
         $db = Core::DB();
         $res = $db -> rawQuery("SELECT name FROM object WHERE $Max_X>'cx'>$Min_X AND $Max_Y>'cy'>$Min_Y");
+        $msg = $db->getLastError();
+        echo  $msg;
         echo json_encode($res);
         return $res;
     }
